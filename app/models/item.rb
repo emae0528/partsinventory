c30e4c1d4ffde7d59c6b76a117f24cdb55a9b320
Raphael B. Alampay <raphael.alampay@gmail.com>
2013-09-12 17:17:58 +0800

    Inclusion of possible status values

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ActiveRecord::Base
   attr_accessible :comments, :member_id, :name, :price, :quantity, :source, :status
 
+  STATUS = ["Need", "Pending", "Order Complete", "Delivered"]
+
   validates :name, presence: true
   validates :price, presence: true, numericality: true
   validates :quantity, presence: true, numericality: true
   validates :source, presence: true
-  validates :status, presence: true
+  validates :status, presence: true, inclusion: { in: STATUS }
 
   belongs_to :member
 end
