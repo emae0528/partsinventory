02d226dfc801538ea946e50b09f5e5c95e503474
Hannah Grace S. Bulatao <bulataohannahgrace@yahoo.com>
2013-09-12 16:06:42 -0700

    Hannah's Changes 2

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -12,4 +12,13 @@ class MembersController < ApplicationController
   	@member = Member.find(params[:id])
   end
 
+  def create 
+  	@member = Member.new(params[:member])
+		
+		if @member.save
+			redirect_to member_path(params[:id])
+		else
+			render :new
+		end
+  end
 end
