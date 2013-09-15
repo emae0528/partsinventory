d94357a4ac66c47d10338755fb0b733df2bcf944
emae <emae0528>
2013-09-15 15:51:25 +0800

    color coding item status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def get_color_of_status(status)
+		case(status)
+		when "Need" then "red"
+		when "Pending" then "orange"
+		when "Order Complete" then "green"
+		when "Delivered" then "blue"
+		else "white" end
+	end
 end
